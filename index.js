528be3c8432391f0df00981d85d90e132ecca3a4
Michael Ocheje <mocheje@c2gconsulting.com>
2017-11-17 09:00:10 +0100

    added beneficiary

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ const sendSms = require('./shared/lib/sms').sendSms
 //load modules for data persistence
 const cache = require('./shared/lib/cache').getRedisClient();
 const db = require('./shared/lib/db');
+const _ = require('underscore');
 
 
 // Connect to our mongo database
@@ -41,7 +42,7 @@ io.on("connection", socket => {
     console.log("New client connected");
     socket.on("disconnect", () => console.log("Client disconnected"));
     socket.on("clientRequest", data => {
-        const request = DF.textRequest(data, {sessionId: '7u8y7678u'});
+        const request = DF.textRequest(data, {sessionId: '7u097678u'});
         request.on('response', function(response) {
             // do all computations here for now.
             processResponse(response, function(err, data){
@@ -72,7 +73,7 @@ function processResponse(res, callback) {
             const phone = res.result.parameters['phone-number'];
             console.log(phone);
             if(phone){
-                User.findOne({'phone': phone})
+                User.findOne({'phone': String(phone)})
                     .then(x => {
                         if(x){
                             sendSms(`your token is ${Math.ceil(Math.random()*100000)}`, phone);
@@ -93,6 +94,38 @@ function processResponse(res, callback) {
             const token = res.result.parameters.token;
             res.result.fulfillment.speech = `Token ${token} correct and your account balance is 60000.009 NGN`;
             callback('', res)
+            break;
+        case 'fund_transfer':
+            const { account, amount, beneficiary } = res.result.parameters;
+            console.log(account, amount, beneficiary);
+            if(account && amount && beneficiary){
+                User.findOne({'account.number': String(account)})
+                    .then(x => {
+                        if(x){
+                            //check if user has the beneficiary
+                            const name = new RegExp(beneficiary);
+                            const ben = x.beneficiaries.filter(f => (f.fullname.match(name)) !== null);
+                            if(ben && ben.length == 1){
+                                //transfer money to this beneficiary
+                                res.result.fulfillment.speech = `${amount} transferred to ${ben[0].fullname} and your current balance is ${60000.009 - amount}`
+                            } else if(ben && ben.length == 1){
+                                res.result.fulfillment.speech = `${amount} has been transferred to ${beneficiary} from ${account}`
+                            } else {
+                                res.result.fulfillment.speech = `Beneficiary not found Do you want to add ${beneficiary} as a beneficiary`
+                            }
+                            callback('', res);
+                        } else {
+                            res.result.fulfillment.speech = `Account number ${account} does not exist`;
+                            callback('', res)
+                        }
+                    })
+            } else {
+                callback('', res)
+            }
+            break;
+        case 'fund_transfer - add - beneficiary':
+            res.result.fulfillment.speech = `Beneficiary Added`;
+            callback('', res);
             break;
         default :
             res.result.fulfillment.speech = res.result.fulfillment.speech || `Processing... you can go ahead and ask other things while i'm running my queries`
